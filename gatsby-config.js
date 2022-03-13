f1ac6e535fdb73d4f5172eb96c12b575121b395e
Jakub Wojtyra <jakubwojtyra@MacBook-Air-Jakub.local>
2022-03-13 12:03:39 +0100

    Updated gatsby-remark-relative-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,10 +44,7 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: "gatsby-remark-relative-images",
-            options: {
-              name: "uploads",
-            },
+            resolve: "gatsby-remark-relative-images"
           },
           {
             resolve: "gatsby-remark-images",
